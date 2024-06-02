e1ef6d844beb30c4112c251abe020bf332ce3ba1
darkryh_ <alex_torres-xc@hotmail.com>
2024-06-01 21:19:04 -0500

    example of use case

--- app/src/main/java/com/ead/app/somoskudasai/MainActivity.kt
+++ app/src/main/java/com/ead/app/somoskudasai/MainActivity.kt
@@ -1,21 +1,49 @@
 package com.ead.app.somoskudasai
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.ViewGroup
+import android.webkit.WebView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import coil.transform.CircleCropTransformation
 import com.ead.app.somoskudasai.main.MainViewModel
-import com.ead.app.somoskudasai.ui.theme.SomosKudasaiTheme
+import com.ead.app.somoskudasai.main.theme.SomosKudasaiTheme
+import com.ead.app.somoskudasai.util.components.ImageLoader
+import com.ead.lib.somoskudasai.models.News
+import com.ead.lib.somoskudasai.models.html_tags.H1
+import com.ead.lib.somoskudasai.models.html_tags.H2
+import com.ead.lib.somoskudasai.models.html_tags.H3
+import com.ead.lib.somoskudasai.models.html_tags.H4
+import com.ead.lib.somoskudasai.models.html_tags.H5
+import com.ead.lib.somoskudasai.models.html_tags.Iframe
+import com.ead.lib.somoskudasai.models.html_tags.Image
+import com.ead.lib.somoskudasai.models.html_tags.P
+import com.ead.lib.somoskudasai.models.html_tags.Ul
+import com.ead.lib.somoskudasai.models.html_tags.Video
 
 class MainActivity : ComponentActivity() {
 
@@ -30,9 +58,9 @@ class MainActivity : ComponentActivity() {
                     viewModel.onEvent()
                 }
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
+                    NewsComposable(
                         modifier = Modifier.padding(innerPadding),
-                        result = viewModel.result.value
+                        news = viewModel.news.value
                     )
                 }
             }
@@ -41,21 +69,207 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(modifier: Modifier = Modifier, result : String? = null) {
-    LazyColumn {
-        item {
-            Text(
-                text = result ?:"null",
-                modifier = modifier
-            )
+fun NewsComposable(modifier: Modifier = Modifier, news: News?) {
+    LazyColumn(
+        modifier = modifier,
+    ) {
+        /**
+         * Loading Metadata
+         */
+        if (news != null) {
+            item {
+                Text(
+                    text = "Metadata",
+                    style = MaterialTheme.typography.headlineSmall,
+                    modifier = Modifier.fillMaxWidth(),
+                    textAlign = TextAlign.Center
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+            }
+            item {
+                ImageLoader(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(200.dp),
+                    url = news.image
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+            }
+            item {
+                Text(
+                    text = "title : ${news.title}",
+                    style = MaterialTheme.typography.bodyLarge
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+            }
+            item {
+                Text(
+                    text = "date : ${news.date}",
+                    style = MaterialTheme.typography.bodyLarge
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+            }
+            item {
+                Text(
+                    text = "type : ${news.type}",
+                    style = MaterialTheme.typography.bodyLarge
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+            }
+            item {
+                Text(
+                    text = "Author",
+                    style = MaterialTheme.typography.headlineSmall,
+                    modifier = Modifier.fillMaxWidth(),
+                    textAlign = TextAlign.Center
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+            }
+            item {
+                Row {
+                    ImageLoader(
+                        url = news.authorImage,
+                        modifier = Modifier
+                            .height(60.dp)
+                            .width(60.dp),
+                        contentScale = ContentScale.Crop,
+                        builder = {
+                            CircleCropTransformation()
+                        }
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Text(
+                        text = "author : ${news.author}",
+                        style = MaterialTheme.typography.bodyLarge
+                    )
+                }
+                Spacer(modifier = Modifier.height(8.dp))
+            }
+            item {
+                Text(
+                    text = "authorWords : ${news.authorWords}",
+                    style = MaterialTheme.typography.bodyLarge
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+            }
+            item {
+                Text(
+                    text = "News Details",
+                    style = MaterialTheme.typography.headlineSmall,
+                    modifier = Modifier.fillMaxWidth(),
+                    textAlign = TextAlign.Center
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+            }
+
+            /**
+             * Loading news details using html tags Instance
+             */
+            items(news.structure) { tag ->
+                when (tag) {
+                    is H1 -> {
+                        Text(
+                            text = tag.text,
+                            style = MaterialTheme.typography.headlineSmall
+                        )
+                    }
+                    is H2 -> {
+                        Text(
+                            text = tag.text,
+                            style = MaterialTheme.typography.headlineSmall
+                        )
+                    }
+                    is H3 -> {
+                        Text(
+                            text = tag.text,
+                            style = MaterialTheme.typography.headlineSmall
+                        )
+                    }
+                    is H4 -> {
+                        Text(
+                            text = tag.text,
+                            style = MaterialTheme.typography.headlineSmall
+                        )
+                    }
+                    is H5 -> {
+                        Text(
+                            text = tag.text,
+                            style = MaterialTheme.typography.headlineSmall
+                        )
+                    }
+                    is P -> {
+                        Text(
+                            text = tag.text,
+                            style = MaterialTheme.typography.bodyLarge
+                        )
+                    }
+                    is Iframe -> {
+                        WebViewComposable(url = tag.url)
+                    }
+                    is Image -> {
+                        ImageLoader(
+                            url = tag.src,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(300.dp),
+                            contentScale = ContentScale.Crop
+                        )
+                    }
+                    is Ul -> {
+                        tag.items.forEach { text ->
+                            Text(
+                                text = text,
+                                style = MaterialTheme.typography.bodyLarge,
+                                color = Color.DarkGray
+                            )
+                            Spacer(modifier = Modifier.height(8.dp))
+                        }
+                    }
+                    is Video -> {
+                        Text(
+                            text = "url of video : ${tag.src}",
+                            style = MaterialTheme.typography.bodyLarge
+                        )
+                    }
+                }
+
+                Spacer(modifier = Modifier.height(12.dp))
+            }
         }
     }
+}
+
+/**
+ * WebViewComposable to load Iframes objects
+ */
+@SuppressLint("SetJavaScriptEnabled")
+@Composable
+fun WebViewComposable(modifier: Modifier = Modifier, url: String) {
+    AndroidView(
+        modifier = modifier
+            .height(300.dp)
+            .fillMaxWidth(),
+        factory = { context ->
+
+            WebView(context).apply {
+                layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT
+                )
+                settings.domStorageEnabled = true
+                settings.javaScriptEnabled = true
+                loadUrl(url)
+            }
+        }
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     SomosKudasaiTheme {
-        Greeting()
+        NewsComposable(
+            news = null
+        )
     }
 }
--- app/src/main/java/com/ead/app/somoskudasai/main/MainViewModel.kt
+++ app/src/main/java/com/ead/app/somoskudasai/main/MainViewModel.kt
@@ -1,27 +1,39 @@
 package com.ead.app.somoskudasai.main
 
-import android.util.Log
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.ead.lib.somoskudasai.SomosKudasai
+import com.ead.lib.somoskudasai.models.News
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
-    private val _result : MutableState<String?> = mutableStateOf(null)
-    val result : State<String?> = _result
+    /**
+     * news from loading page of SomosKudasai
+     */
+    private val _news : MutableState<News?> = mutableStateOf(null)
+    val news : State<News?> = _news
 
+    /**
+     * Handling event to load from home page
+     * and load the first recent news page
+     * with details
+     */
     fun onEvent() {
         viewModelScope.launch (Dispatchers.IO) {
-            val pageDetails = SomosKudasai.getNewsInfo("https://somoskudasai.com/noticias/cultura-otaku/whisper-me-a-love-song-es-la-peor-produccion-de-la-temporada")
-            _result.value = pageDetails?.structure?.map { it.value }.toString()
 
-            pageDetails?.structure?.forEach {
-                Log.d("test", "onEvent: ${it.value}")
-            }
+            val home = SomosKudasai.getHome()
+
+            val recentNews  = home?.recentNews ?: return@launch
+
+            val firstNews = recentNews.first()
+
+            val newsDetail = SomosKudasai.getNewsInfo(firstNews.url)
+            _news.value = newsDetail
+
         }
     }
 }
